e9303c9f11726658a13c327d9371b925a904b897
Ganeshmoorthi <33057527+ganeshhappysanz@users.noreply.github.com>
2022-02-08 08:56:43 +0530

    pageobject updated

--- com.FreeSpaceAssignment/src/main/java/pageElements/PageObjects.java
+++ com.FreeSpaceAssignment/src/main/java/pageElements/PageObjects.java
@@ -8,7 +8,18 @@ public class PageObjects {
 	@FindBy(xpath="//input[@name='q'] | //input[@title=('Search')]")
 	public static WebElement searchTextbox;
 	
-	@FindBy(xpath="//*[contains(text(),'Rating')  and  //*[contains(text(),'Zomato: Food Delivery & Dining – Apps on Google Play')]]")
+	@FindBy(xpath="//*[contains(text(),'Rating')  and  //*[contains(text(),'Zomato: Food Delivery & Dining ? Apps on Google Play')]]")
 	public static WebElement zomatoPlaystore;
+	
+	@FindBy(xpath="//*[contains(text(),'Rating') and //*[contains(text(),'Swiggy : Food Delivery & More – Apps on Google Play')]]")
+	public static WebElement swiggyPlaystore;
+	
+	@FindBy(xpath="//*[contains(text(),'Rating') and //*[contains(text(),'Netflix – Apps on Google Play')]]")
+	public static WebElement netflixPlaystore;
+	
+	@FindBy(xpath="//*[contains(text(),'Rating') and //*[contains(text(),'ZEE5: Movies, TV Shows, Web Series, News - Google Play')]]")
+	public static WebElement zee5Playstore;
+	
+	
 
 }
