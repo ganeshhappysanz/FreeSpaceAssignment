c04d1ab5386d36d9b5bfbf9bcef9b67a112b9a17
Ganeshmoorthi <33057527+ganeshhappysanz@users.noreply.github.com>
2022-02-15 10:40:26 +0530

    Driver updated

--- com.FreeSpaceAssignment/src/main/java/broswerSetup/Setupbrowser.java
+++ com.FreeSpaceAssignment/src/main/java/broswerSetup/Setupbrowser.java
@@ -19,61 +19,55 @@ import org.testng.annotations.BeforeSuite;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Setupbrowser {
-	
-	public static WebDriver driver=null;
-	public static Properties properties=null;
-	
-	
+
+	public static WebDriver driver = null;
+	public static Properties properties = null;
+
 	public static Properties loadpropertyFile() throws IOException {
-        FileInputStream fileInputStream=new FileInputStream("config.properties");
-        properties =new Properties();
-        properties.load(fileInputStream);
-        return properties;
-    }
-	
-	public static void ElementWait() throws Exception{ 
-		new WebDriverWait(driver, 60).until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[contains(text(),'Rating')]"),"Rating"));
+		FileInputStream fileInputStream = new FileInputStream("config.properties");
+		properties = new Properties();
+		properties.load(fileInputStream);
+		return properties;
 	}
 
-	
-			
-		@BeforeSuite
-		public static void setup() throws IOException {
-			 loadpropertyFile();
-		     String Appurl=properties.getProperty("APPURL");
-		     String Browsers=properties.getProperty("browser");
-			//WebDriverManager.firefoxdriver().setup();
-		     
-			 
-			 
-		 	 if(Browsers.equalsIgnoreCase("chrome")) {
-			 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
-			 driver=new ChromeDriver();
-		 	 }else if(Browsers.equalsIgnoreCase("fireFox")) {
-		 		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\Drivers\\geckodriver.exe");
-				 driver=new FirefoxDriver();
-		 	 }
-		 	 driver.manage().window().maximize();
-		 	 driver.get(Appurl);
-			
+	public static void ElementWait() throws Exception {
+		new WebDriverWait(driver, 60).until(ExpectedConditions
+				.textToBePresentInElementLocated(By.xpath("//*[contains(text(),'Rating')]"), "Rating"));
+	}
+
+	@BeforeSuite
+	public static void setup() throws IOException {
+		loadpropertyFile();
+		String Appurl = properties.getProperty("APPURL");
+		String Browsers = properties.getProperty("browser");
+		// WebDriverManager.firefoxdriver().setup();
+
+		if (Browsers.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\Drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (Browsers.equalsIgnoreCase("fireFox")) {
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\Drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
 		}
-		
-		public static void pageScrollDown() {
-			
-			  JavascriptExecutor jse = (JavascriptExecutor)driver;
-			  jse.executeScript("window.scrollBy(0,1800)");
-			  jse.executeScript("window.scrollBy(0,-550)");
-			 
-			
-		}
-		
-		
-		@AfterSuite
-		public void teardown() {
-			if(driver!=null) {
-	            driver.quit();
-	        }
-		}
-		
+		driver.manage().window().maximize();
+		driver.get(Appurl);
 
 	}
+
+	public static void pageScrollDown() {
+
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("window.scrollBy(0,1800)");
+		jse.executeScript("window.scrollBy(0,-550)");
+
+	}
+
+	@AfterSuite
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+}
